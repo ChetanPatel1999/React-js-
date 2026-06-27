3aa753aaa877a13a58d1507b3b9f997f3e731edd
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-06-27 12:03:59 +0530

    add

---
+++ 18_localStorage/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const App = () => {
+  // localStorage.setItem("age",18)
+  // let data = localStorage.getItem("user");
+  // console.log(data);
+  // localStorage.removeItem("age")
+  // localStorage.clear();
+  // let user = { name: "ram", age: 5 }
+  // localStorage.setItem("data", JSON.stringify(user)); // to convert in string
+
+  // let data =JSON.parse(localStorage.getItem("data"));
+  // console.log(data)
+
+  // let arr=[12,34,56,78]
+  // localStorage.setItem("myarray",JSON.stringify(arr));
+
+  // let arr = JSON.parse(localStorage.getItem("myarray"));
+  // console.log(arr)
+
+  //  console.log(localStorage.length)
+
+  // console.log(localStorage.key(0))
+  // console.log(localStorage.key(1))
+
+  return (
+    <div>
+      APP
+    </div>
+  )
+}
+
+export default App
